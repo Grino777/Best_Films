bef56d10dad3da58e07af168507fb9d325ce77b5
Grino777 <bars_B_P@mail.ru>
2023-04-13 13:58:28 +0300

    Added models.py

--- best_films/films_app/admin.py
+++ best_films/films_app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['category_name']
+
+    def __str__(self):
+        return 'Категории'
+
+    class Meta:
+        verbose_name = 'Категории'
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['movie_title', 'original_title', 'category']
+
+@admin.register(Status)
+class StatusAdmin(admin.ModelAdmin):
+    list_display = ['status']
--- best_films/films_app/models.py
+++ best_films/films_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=50, verbose_name='Категория')
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+class Movie(models.Model):
+    movie_title = models.CharField(max_length=100, verbose_name='Название')
+    original_title = models.CharField(max_length=100, blank=True, default='', verbose_name='Оригинальное название')
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name='Жанр')
+
+    def __str__(self):
+        return self.movie_title
+
+class Status(models.Model):
+    status = models.CharField(max_length=20, verbose_name='Статус')
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
